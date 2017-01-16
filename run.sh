db5b1c27e1e985ad2a41326e85ed6752b27b22e5
dennis.huning@audi.de <dennis.huning@audi.de>
2017-01-16 07:58:38 +0100

    Use docker to run example with kafka or rabbitmq.

--- run.sh
+++ run.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 1 ]
+then
+    echo "Usage: 'run.sh DIRECTORY'"
+    echo "e.g. ./run.sh docker/rabbitmq"
+    exit 1
+fi
+
+path=$1"docker-compose.yml"
+projectName="spring-stream"
+
 set -e
 
 # Build the project and docker images
 mvn clean package
 
 # Remove existing containers
-docker-compose stop
-docker-compose rm -f
 
-docker-compose up -d
+docker-compose -p $projectName -f $path stop
+docker-compose -p $projectName -f $path rm -f
 
-docker-compose logs
+docker-compose -p $projectName -f $path up -d
+
+docker-compose -p $projectName -f $path logs
